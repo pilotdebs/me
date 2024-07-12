c84c35e1514c563e041fe29550aea17421294ffa
pilotdebs <pilotdebs@gmail.com>
2024-07-12 18:55:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function changeText() {
+    var paragraph = document.querySelector('#app p');
+    paragraph.textContent = 'Text changed!';
+}
+
+
+Devoted Community Mod  (Head Mod @Coin Raiders),
+Crypto Enthusiast, Resourceful, Diligent, Quest-loving, Quote-loving
+
+0x63a81b926da0b80FE1d6ea9AD6D6439DEd54c7E3
+
+Crypto trader, web3 enthusiast,fun loving, Resourceful, Diligent
